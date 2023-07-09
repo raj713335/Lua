627513352ed99a561fea839cc54cd3e48d0008d7
ARNAB DAS <raj713335@gmail.com>
2023-07-09 20:26:48 +0530

    :wrenc:h:  Loops in Lua

---
+++ Loops.lua
@@ -0,0 +1,39 @@
+
+for i = 100, 10, -1 do 
+    print(i)
+end
+
+print(i)
+
+local start_val, end_val, step_val = 1, 10, 1
+
+for i = start_val, end_val, step_val do 
+    print(i)
+end
+
+print(i)
+
+local arr = { 2, 3, 45, 77, 432, 999 }
+
+for i = 1, #arr do
+    print(arr[i])
+end
+
+local peeps = 10
+
+while peeps > 0 do
+    peeps = peeps - 1
+    print("People left at party : " .. peeps)
+end
+
+local x = 11
+
+while x < 10 do
+    print("hey there")
+    x = x + 1
+end
+
+repeat 
+    print("hey there")
+    x = x -1
+until x > 1
